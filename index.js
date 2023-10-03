92512a53875b34b6f8938ad32172c0370389d67f
Karim Rejani <karim.rjn02@gmail.com>
2023-10-03 10:29:33 +0200

    Adding the files of the project

---
+++ index.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+
+console.log(
+    chalk.black("Ange")
+)
+
+console.log(
+    chalk.red("Akane")
+)
+
+console.log(
+    chalk.magenta("Caroline")
+)
+
+console.log(
+    chalk.blue("Sissis")
+)
